879a74724620330bf07410a8daea93b94359deb2
vista-54 <vista-54@ukr.net>
2016-07-27 15:58:15 +0300

    fixed: -some bugs

--- common/directives/calendar/calendar.js
+++ common/directives/calendar/calendar.js
@@ -3,56 +3,54 @@
  */
 main.directive('calendar', ['$rootScope',
     function ($rootScope) {
-    return {
-        restrict: 'A',
-        templateUrl: 'common/directives/calendar/calendar.html',
-        link: function (scope, element, attrs) {
-            // console.log('Calendarlink');
-            // console.log(scope.month);
-        },
-        controller: 'calendarCtrl'
-    };
-}]);
+        return {
+            restrict: 'A',
+            templateUrl: 'common/directives/calendar/calendar.html',
+            link: function (scope, element, attrs) {
+                // console.log('Calendarlink');
+                // console.log(scope.month);
+            },
+            controller: 'calendarCtrl'
+        };
+    }]);
 
 main.directive('day', ['$rootScope', 'calendarModel',
     function ($rootScope, calendarModel) {
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
-                element.on("click", function () {
-                    if(element.hasClass('dayLast')){
+                element.on("click", function (e) {
+                    if (element.children().hasClass('dayLast')) {
                         return false;
                     }
-                    calendarModel.selectedSheduleDay=parseInt(element.children().text());
-                    calendarModel.selectedSheduleMonth=parseInt(element.children().attr('data-month'));
-                    // console.log(calendarModel.selectedSheduleDay);
-                    // console.log(scope.qqq);
+                    console.log(element);
+                    calendarModel.selectedSheduleDay = parseInt(element.children().text());
+                    calendarModel.selectedSheduleMonth = parseInt(element.children().attr('data-month'));
                     var elemArr = angular.element(document).find('.whiteBorder');
                     elemArr.removeClass('whiteBorder');
                     element.addClass('whiteBorder');
-
-
+                //
                 })
             },
             controller: 'calendarCtrl'
         };
     }]);
-main.directive('switcher', ['$sessionStorage', '$rootScope','calendarModel',
-    function ($sessionStorage, $rootScope,calendarModel) {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
+main.directive('switcher', ['$sessionStorage', '$rootScope', 'calendarModel',
+    function ($sessionStorage, $rootScope, calendarModel) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
 
-            element.on("click", function () {
-                var elemArr = angular.element(document).find('.whiteBorder');
-                elemArr.removeClass('whiteBorder');
-                if (typeof $rootScope.selectedDay !== 'undefined') {
-                    var selectDay = angular.element(document).find('li[name=day] span[data-day=' + $rootScope.selectedDay.day + '][data-month=' + $rootScope.selectedDay.month + ']')
-                }
-                console.log(calendarModel.selectedDay);
-                // element.addClass('whiteBorder');
-            })
-        },
-        controller: 'calendarCtrl'
-    };
-}]);
+                element.on("click", function () {
+                    var elemArr = angular.element(document).find('.whiteBorder');
+                    elemArr.removeClass('whiteBorder');
+                    if (typeof $rootScope.selectedDay !== 'undefined') {
+                        var selectDay = angular.element(document).find('li[name=day] span[data-day=' + $rootScope.selectedDay.day + '][data-month=' + $rootScope.selectedDay.month + ']')
+                    }
+                    console.log(calendarModel.selectedDay);
+                    // element.addClass('whiteBorder');
+                })
+            },
+            controller: 'calendarCtrl'
+        };
+    }]);
